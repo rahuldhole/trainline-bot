1ebde177dfb7e16cfdb15adaedb30acf98c0cff8
rahuldhole <rdhole95@gmail.com>
2023-12-18 13:37:49 +0100

    Initial commit for orphan branch

--- config/applications.rb
+++
@@ -1,1 +0,0 @@
-Dir.glob(File.join(File.dirname(__FILE__), '../app/**/*.rb'), &method(:require))
--- spec/spec_helper.rb
+++
@@ -1,9 +0,0 @@
-require_relative '../config/applications.rb'
-require 'colorize'
-
-RSpec.configure do |config|
-  config.color = true
-  config.tty = true
-  config.formatter = :documentation
-  config.include Capybara::DSL
-end
