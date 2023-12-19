7ea1872b71000924ee0ef0cf5894d34409fcb7a6
rahuldhole <rdhole95@gmail.com>
2023-12-19 04:09:25 +0100

    Add Datadom Captcha Bypass Configuration

--- app/lib/api_scrapper.rb
+++ app/lib/api_scrapper.rb
@@ -32,61 +32,133 @@ module ApiScrapper
     return nil
   end
 
-  def self.test_make_json_post_request_with_headers
-    url = URI("https://www.thetrainline.com/api/journey-search/")
+  # TODO: Datadom Captcha ByPass Configuration is required to get a proper response from https://www.thetrainline.com/api/journey-search/
+  # def self.test_make_json_post_request_with_headers
+  #   proxy_host = '45.129.126.61'
+  #   proxy_port = '50100'
+  #   proxy_user = 'rdhole95'
+  #   proxy_pass = ''
+  #   url = URI("https://www.thetrainline.com/api/journey-search/")
     
-    https = Net::HTTP.new(url.host, url.port)
-    https.use_ssl = true
+  #   https = Net::HTTP.new(url.host, url.port, proxy_host, proxy_port, proxy_user, proxy_pass)
+  #   https.use_ssl = true
     
-    request = Net::HTTP::Post.new(url)
-    request["sec-ch-ua"] = "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\""
-    request["x-version"] = "4.35.27635"
-    request["sec-ch-ua-mobile"] = "?0"
-    request["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
-    request["Content-Type"] = "application/json"
-    request["accept"] = "application/json"
-    request["sec-ch-ua-platform"] = "\"Linux\""
-    request["Sec-Fetch-Site"] = "same-origin"
-    request["Sec-Fetch-Mode"] = "cors"
-    request["Sec-Fetch-Dest"] = "empty"
-    request["host"] = "www.thetrainline.com"
-    request["Cookie"] = "datadome=mysGPdCq2R2l9HQwVdflFyuRpCA3lanq45tWWdwkLP2mvj~fxRx8l~wPObk1cLaxRtj2kmOWv2TDW5m1Th5N~Gy2mZp5xwHviUpxU3VgIVF8DoPUuzOXaQNOJ~bMyZ88"
-    request.body = JSON.dump({
-      "passengers": [
-        {
-          "id": "efd67d6c-8a28-4ccc-8258-0b59e1ebed69",
-          "dateOfBirth": "1990-12-18",
-          "cardIds": []
-        }
-      ],
-      "isEurope": true,
-      "cards": [],
-      "transitDefinitions": [
-        {
-          "direction": "outward",
-          "origin": "urn:trainline:generic:loc:5306",
-          "destination": "urn:trainline:generic:loc:827",
-          "journeyDate": {
-            "type": "departAfter",
-            "time": "2023-12-22T14:15:00"
-          }
-        }
-      ],
-      "type": "single",
-      "maximumJourneys": 5,
-      "includeRealtime": true,
-      "transportModes": [
-        "mixed"
-      ],
-      "directSearch": false,
-      "composition": [
-        "through",
-        "interchangeSplit"
-      ]
-    })
+  #   request = Net::HTTP::Post.new(url)
+  #   # request["sec-ch-ua"] = "\"Not_A Brand\";v=\"8\", \"Chromium\";v=\"120\", \"Google Chrome\";v=\"120\""
+  #   # request["x-version"] = "4.35.27635"
+  #   # request["sec-ch-ua-mobile"] = "?0"
+  #   request["User-Agent"] = "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36"
+  #   # request["Content-Type"] = "application/json"
+  #   request["accept"] = "application/json"
+  #   # request["sec-ch-ua-platform"] = "\"Linux\""
+  #   # request["Sec-Fetch-Site"] = "same-origin"
+  #   # request["Sec-Fetch-Mode"] = "cors"
+  #   # request["Sec-Fetch-Dest"] = "empty"
+  #   # request["host"] = "www.thetrainline.com"
+  #   # request["Cookie"] = "datadome=mysGPdCq2R2l9HQwVdflFyuRpCA3lanq45tWWdwkLP2mvj~fxRx8l~wPObk1cLaxRtj2kmOWv2TDW5m1Th5N~Gy2mZp5xwHviUpxU3VgIVF8DoPUuzOXaQNOJ~bMyZ88"
+  #   request.body = JSON.dump({
+  #     "passengers": [
+  #       {
+  #         "id": "efd67d6c-8a28-4ccc-8258-0b59e1ebed69",
+  #         "dateOfBirth": "1990-12-18",
+  #         "cardIds": []
+  #       }
+  #     ],
+  #     "isEurope": true,
+  #     "cards": [],
+  #     "transitDefinitions": [
+  #       {
+  #         "direction": "outward",
+  #         "origin": "urn:trainline:generic:loc:5306",
+  #         "destination": "urn:trainline:generic:loc:827",
+  #         "journeyDate": {
+  #           "type": "departAfter",
+  #           "time": "2023-12-22T14:15:00"
+  #         }
+  #       }
+  #     ],
+  #     "type": "single",
+  #     "maximumJourneys": 5,
+  #     "includeRealtime": true,
+  #     "transportModes": [
+  #       "mixed"
+  #     ],
+  #     "directSearch": false,
+  #     "composition": [
+  #       "through",
+  #       "interchangeSplit"
+  #     ]
+  #   })
     
-    response = https.request(request)
-    puts response.read_body    
-  end
+  #   response = https.request(request)
+  #   captcha_url = JSON.parse(response.read_body)["url"]
+  #   puts "captcha_url: #{captcha_url.inspect}".colorize(:green)
+  #   solveCapctha(captcha_url, url.host)
+  # end
 
+  # def self.solveCapctha(captcha_url, web_url)
+  #   proxy_host = '45.129.126.61'
+  #   proxy_port = '50100'
+  #   proxy_user = 'rdhole95'
+  #   proxy_pass = ''
+  #   puts "Solving captcha...".colorize(:yellow)
+  #   captcha_api_key = ""
+  
+  #   url = URI("https://api.2captcha.com/createTask")
+  
+  #   https = Net::HTTP.new(url.host, url.port, proxy_host, proxy_port, proxy_user, proxy_pass)
+  #   https.use_ssl = true
+  
+  #   request = Net::HTTP::Post.new(url.path)
+  #   request["Content-Type"] = "application/json"
+  #   request["accept"] = "application/json"
+  #   request.body = JSON.dump({
+  #     "clientKey": captcha_api_key,
+  #     "task": {
+  #       "type": "DataDomeSliderTask",
+  #       "websiteURL": web_url,
+  #       "captchaUrl": captcha_url,
+  #       "userAgent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36",
+  #       "proxyType":"https",
+  #       "proxyAddress":"#{proxy_host}",
+  #       "proxyPort": "#{proxy_port}",
+  #       "proxyLogin":"#{proxy_user}",
+  #       "proxyPassword":"#{proxy_pass}"
+  #     }
+  #   })
+  
+  #   response = https.request(request)
+  #   puts "response=#{response.read_body}".colorize(:green)
+  #   captcha_id = JSON.parse(response.read_body)["taskId"]
+  #   puts "captcha_id: #{captcha_id.inspect}".colorize(:green)
+  #   sleep(60)
+  #   puts "Getting captcha solution...".colorize(:yellow)
+  #   url = URI("https://api.2captcha.com/getTaskResult")
+  
+  #   https = Net::HTTP.new(url.host, url.port, proxy_host, proxy_port, proxy_user, proxy_pass)
+  #   https.use_ssl = true
+  
+  #   request = Net::HTTP::Post.new(url.path)
+  #   request["Content-Type"] = "application/json"
+  #   request["accept"] = "application/json"
+  #   request.body = JSON.dump({
+  #     "clientKey": captcha_api_key,
+  #     "taskId": "#{captcha_id}"
+  #   })
+  
+  #   response = https.request(request)
+  #   puts "response=#{response.read_body}".colorize(:green)
+  
+  #   if JSON.parse(response.read_body)["solution"].nil?
+  #     puts "Did not get captcha solution yet, retry...".colorize(:cyan)
+  #   else
+  #     captcha_solution = JSON.parse(response.read_body)["solution"]["token"]
+  #     puts "captcha_solution: #{captcha_solution.inspect}".colorize(:green)
+  #     return captcha_solution
+  #   end
+  
+  # rescue StandardError => e
+  #   puts "HTTP Request failed (#{e.message})".colorize(:red)
+  #   return nil
+  # end
 end
